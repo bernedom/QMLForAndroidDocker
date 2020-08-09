3b48962a3d9a7a37bcba0f44a237f8a08d34224d
Dominik Berner <dominik.berner@gmail.com>
2020-08-09 22:34:11 +0200

    Add build script for container

--- run_docker.sh
+++ run_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -ti -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix androidqmlondocker
+docker run --rm -ti -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix androidqmlondocker
